11f3c6a773ee1554519b86412f22085c83df144d
Vemark1101 <vemarkapostadero@gmail.com>
2026-03-29 20:43:52 -0700

    Create navbar.php

---
+++ php5/views/includes/navbar.php
@@ -0,0 +1,8 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
+    <div class="container">
+        <a class="navbar-brand fw-bold" href="<?php echo APP_URL; ?>">Faculty MVC CRUD</a>
+        <span class="navbar-text text-light">
+            PHP Output No. 5
+        </span>
+    </div>
+</nav>
